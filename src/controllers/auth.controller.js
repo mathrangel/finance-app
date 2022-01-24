97e13c391f2ccf90ff96266f4c5faf674054c974
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-01-24 15:31:36 -0300

    add user store method

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const knex = require('../database')
-
+const bcrypt = require('bcrypt')
 
 module.exports = {
     async get(req, res, next) {
@@ -11,7 +11,28 @@ module.exports = {
     },
     async store(req, res, next) {
         try {
-            res.send({msg: 'METHOD STORE'})
+            const {
+                name,
+                password,
+                confirmPassword,
+                email
+            } = req.body
+        
+            if(password !== confirmPassword) return res.status(401).send('as senhas não correspondem')
+
+            const hashedPassword = await bcrypt.hash(password, 10)
+
+            const emailValidation = knex('users').select().where({ email: email }).first()
+            if(emailValidation) return res.status(400).send('email already exists')
+
+            await knex('users')
+            .insert({
+                name: name,
+                password: hashedPassword,
+                email: email
+            })
+
+            return res.status(201).send('criado com sucesso!')
         } catch (e) {
             console.error(e)
         }
