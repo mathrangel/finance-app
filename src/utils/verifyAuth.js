f5171f77216cb50583f73b560fb6ba964de9b647
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-02-26 15:04:38 -0300

    fix res.send(stauts) deprecated

--- src/utils/verifyAuth.js
+++ src/utils/verifyAuth.js
@@ -4,7 +4,7 @@ module.exports = {
   async tokenVerify(req, res, next) {
       const token = req.headers.authorization.split(' ')[1]
       jwt.verify(token, process.env.JWTWEBTOKEN, (err, data) => {
-        if(err) return res.send(403)
+        if(err) return res.sendStatus(403)
         else {
           req.email = data.email
           next()
