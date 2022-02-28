51fed2600eea04477ec46db8632e5ad82dc9fc4b
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-02-27 23:10:40 -0300

    [user] refactory User model

--- src/models/User.js
+++ src/models/User.js
@@ -5,14 +5,22 @@ module.exports = class User {
     id,
     name,
     email, 
-    password
+    password,
+    created_at,
+    deleted_at
   }) {
     this.id = id,
     this.name = name,
     this.email = email,
     this.password = password
+    this.created_at = created_at,
+    this.deleted_at = deleted_at
   }
-
+  
+  static get() {
+    return knex('users')
+  }
+  
   save() {
     return knex('users')
     .insert({
@@ -20,9 +28,5 @@ module.exports = class User {
       email: this.email,
       password: this.password
     })
-  }
-
-  static get() {
-    return knex('users')
   }
 }
