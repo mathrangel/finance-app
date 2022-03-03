231e5ca9803ac31bec822afb118b69642f473f57
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-03-03 17:10:15 -0300

    [client] create HeaderInitial

---
+++ client/src/components/header_initial.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="d-flex justify-content-around align-items-center">
+    <div>LOGOTIPO</div>
+    <div>
+      <button class="btn btn-outline-success px-3 p-2 m-2">
+        Login
+      </button>
+      <button class="btn btn-success px-3 p-2 m-2">
+        Começar agora
+      </button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default { name: "HeaderInitial" }
+</script>
