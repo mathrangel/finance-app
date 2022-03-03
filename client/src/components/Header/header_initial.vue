dd207f63ccb24c5bc116514a315b0be1009c1201
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-03-03 20:17:42 -0300

    [client] change header initial to components\Header

--- client/src/components/Header/header_initial.vue
+++ client/src/components/Header/header_initial.vue
@@ -2,10 +2,10 @@
   <header class="d-flex justify-content-around align-items-center">
     <div>LOGOTIPO</div>
     <div>
-      <button class="btn btn-outline-success px-3 p-2 m-2">
+      <button class="btn btn-outline-success px-3 p-2 mx-2 my-3">
         Login
       </button>
-      <button class="btn btn-success px-3 p-2 m-2">
+      <button class="btn btn-success px-3 p-2 mx-2 my-3">
         Começar agora
       </button>
     </div>
