fb1e07c021541352e441b084c1971050c0e7041e
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-03-04 21:29:12 -0300

    [client] create footer

---
+++ client/src/components/Footer/footer_initial.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="d-flex justify-content-around align-items-end bg-success">
+    <div class="p-5">
+      1
+    </div>
+    <div class="p-5">
+      2
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterInitial'
+}
+</script>
