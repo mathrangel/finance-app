007ac0b93a2cbe7dfdc1c79d90b9540cedfb02fe
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-03-08 12:29:05 -0300

    [client] create button default component

---
+++ client/src/components/Button/button_default.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+  class="btn btn-success"
+  style="width: 200px"
+  @click="onClick"
+  :type="type">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonDefault',
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+  },
+  methods: {
+    onClick($event) {
+      this.$emit('onClick', $event)
+    }
+  }
+}
+</script>
