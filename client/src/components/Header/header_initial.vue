5166cf949e864a0d6b75bce689994608ac8c8093
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-03-08 22:26:47 -0300

    [client] add routes to create account and login

--- client/src/components/Header/header_initial.vue
+++ client/src/components/Header/header_initial.vue
@@ -2,16 +2,32 @@
   <header class="d-flex justify-content-around align-items-center">
     <div>LOGOTIPO</div>
     <div>
-      <button class="btn btn-outline-success px-3 p-2 mx-2 my-3">
+      <button @click="openModalLogin()" class="btn btn-outline-success px-3 mx-2 my-3">
         Login
       </button>
-      <button class="btn btn-success px-3 p-2 mx-2 my-3">
-        Começar agora
-      </button>
+       <AppButtonDefault
+        @onClick="redirectToRegister()" 
+        label="Começar agora"
+        type="submit">
+      </AppButtonDefault>
     </div>
   </header>
 </template>
 
 <script>
-export default { name: "HeaderInitial" }
+import AppButtonDefault from '../Button/button_default.vue'
+export default { 
+  name: "HeaderInitial",
+  components: {
+    AppButtonDefault,
+  },
+  methods: {
+    redirectToRegister() {
+      this.$router.push('/createaccount')
+    },
+    openModalLogin() {
+      this.$router.push('/login')
+    }
+  }
+ }
 </script>
---
+++ client/src/components/Modal/modal_login.vue
@@ -0,0 +1,71 @@
+<template>
+  <section 
+  id="glass-effect" 
+  class="w-100 h-100 position-absolute d-flex align-items-center justify-content-center">
+    <div 
+    id="bg-modal-login" 
+    class="d-flex flex-column align-items-center justify-content-center">
+      <div>
+        <h3 class="text-success"></h3>
+      </div>
+      <div class="w-100 border-bottom my-4"></div>
+      <div class="mb-3">
+        <h3 class="text-success">Entrar</h3>
+      </div>
+      <AppInputDefault 
+      v-model="email"
+      type="email"
+      placeholder="Digite seu Email"
+      label="Email">
+      </AppInputDefault>
+      <AppInputDefault
+      v-model="password"
+      type="password"
+      placeholder="Digite sua Senha" 
+      label="Senha">
+      </AppInputDefault>
+      <div class="mt-5"></div>
+      <AppButtonDefault 
+      @onClick="login" 
+      label="Entrar"
+      type="submit"></AppButtonDefault>
+      <div class="w-100 border-bottom my-4"></div>
+      <span>Não possui conta? <a @click="redirectToRegister()" href="#">Criar conta</a></span>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppInputDefault from '../Input/input_default.vue'
+import AppButtonDefault from '../Button/button_default.vue'
+export default {
+  name: 'ModalLogin',
+  components: {
+    AppInputDefault,
+    AppButtonDefault
+  },
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      console.log('login')
+    },
+    redirectToRegister() {
+      this.$router.push('/createaccount')
+    },
+  }
+}
+</script>
+
+<style>
+  #bg-modal-login {
+    border: 5px solid #eee;
+    border-radius: 10px;
+    padding: 70px 50px;
+    background-color: #fff;
+  }
+</style>
