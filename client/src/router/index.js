48de322f6a4f579afced9f997164a51888d04375
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-03-09 11:08:09 -0300

    [client] delete routes of login and register

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,25 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";  
 
 import Home from '../pages/Views/view_initial.vue'
-import Register from '../components/Modal/modal_register.vue'
-import Login from '../components/Modal/modal_login.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/createaccount',
-    name: 'Register',
-    component: Register
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
+  }
 ]
 
 const router = createRouter({
