4794074154206af163a54f24375ca9d7cfde794b
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-03-10 11:15:55 -0300

    [client] add route view default for user authenticated

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";  
 
 import Home from '../pages/Views/view_initial.vue'
+import ViewDefault from '../pages/Views/view_default.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/home',
+    name: 'ViewDefault',
+    component: ViewDefault
   }
 ]
 
