4cc42d0fe0ccd675034570f0a8d1a3edb5dda771
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-03-15 20:08:11 -0300

    [client] create card default

---
+++ client/src/components/Card/card_default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <span>{{ label }}</span>
+    <div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardDefault',
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  },
+}
+</script>
--- client/src/pages/Views/view_default.vue
+++ client/src/pages/Views/view_default.vue
@@ -1,12 +1,19 @@
 <template>
   <section>
-    
+    <div class="d-flex">
+      <AppCardDefault class="mx-5" label="Ganhos"></AppCardDefault>
+      <AppCardDefault label="Gastos"></AppCardDefault>
+    </div>
   </section>
 </template>
 
 <script>
+import AppCardDefault from '../../components/Card/card_default.vue'
 export default {
-
+  name: 'ViewDefault',
+  components: {
+    AppCardDefault
+  }
 }
 </script>
 
