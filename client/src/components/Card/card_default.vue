2cebfe9e55bfca67db4879710dab7fe31816c347
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-03-16 20:50:16 -0300

    [client] create a initial card component

--- client/src/components/Card/card_default.vue
+++ client/src/components/Card/card_default.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    <span>{{ label }}</span>
-    <div>
+  <div style="width: 500px">
+    <h6 class="bg-success text-white p-2 ">{{ label }}</h6>
+    <div class="p-2">
+        <slot></slot>
     </div>
   </div>
 </template>
--- client/src/pages/Views/view_default.vue
+++ client/src/pages/Views/view_default.vue
@@ -1,8 +1,23 @@
 <template>
   <section>
     <div class="d-flex">
-      <AppCardDefault class="mx-5" label="Ganhos"></AppCardDefault>
-      <AppCardDefault label="Gastos"></AppCardDefault>
+      <AppCardDefault type="true" class="mx-5" label="Ganhos">
+        <div v-for="(item, index) of items" :key="index" class="d-flex justify-content-around">
+          <div style="width: 150px">
+            <span>
+              {{ item.type }}R${{ item.value }}
+            </span>
+          </div>
+          <div class="w-100">
+            <span>
+              {{ item.title }}
+            </span>
+          </div>
+          <span>
+            {{ item.date }}
+          </span>
+        </div>
+      </AppCardDefault>
     </div>
   </section>
 </template>
@@ -13,6 +28,15 @@ export default {
   name: 'ViewDefault',
   components: {
     AppCardDefault
+  },
+  data() {
+    return {
+      items: [
+        {title: 'Salário', type: '+', value: 10000, date: '15/03/2022'},
+        {title: 'Bonus de aniversário', type: '+', value: 100, date: '12/03/2022'},
+        {title: 'Salário', type: '+', value: 100000, date: '15/03/2022'},
+      ]
+    }
   }
 }
 </script>
