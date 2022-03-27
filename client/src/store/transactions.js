6513c1f1af6d3337a99e3b7e30b901ee66415775
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-03-27 10:09:23 -0300

    [client] fix ajusts in db

--- client/src/store/transactions.js
+++ client/src/store/transactions.js
@@ -20,10 +20,10 @@ export default {
     async ActionSetTransactions({ commit }, payload) {
       commit('SET_TRANSACTIONS', payload)
       
-      const earns = payload.filter(e => e.type_transaction === '+')
+      const earns = payload.filter(e => e.type_transaction_id === 1)
       commit('SET_EARNS', earns)
 
-      const spends = payload.filter(e => e.type_transaction === '-')
+      const spends = payload.filter(e => e.type_transaction_id === 2)
       commit('SET_SPENDS', spends)
     },
   }
