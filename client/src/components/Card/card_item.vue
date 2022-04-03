5da241045836d439ad7f9f593da157cf0df928a8
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-04-02 22:57:54 -0300

    [client] create initial card item

---
+++ client/src/components/Card/card_item.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <div v-for="item of spends.data" :key="item.id" class="my-3">
+      <span class="bg-danger p-1">
+      </span>
+        {{ categories[item.category_id - 1].title }}
+      {{ item.title }}
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CardItem',
+  computed: {
+    spends() {
+      return this.$store.state.transactions.spends
+    },
+    categories() {
+      return this.$store.state.transactions.transactions.types
+    }
+  },
+  mounted() {
+    console.log(this.spends)
+    console.log(this.$store.state.transactions)
+    console.log(this.categories)
+  }
+}
+</script>
