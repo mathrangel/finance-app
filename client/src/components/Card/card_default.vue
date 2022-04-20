2e49f431facc4ac57a93dcbf67b8cc66bb8ad437
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-04-19 22:27:04 -0300

    [client] refactorying card default

--- client/src/components/Card/card_default.vue
+++ client/src/components/Card/card_default.vue
@@ -2,23 +2,17 @@
   <div style="width: 400px">
     <h6 v-if="!error" class="d-flex justify-content-between align-items-center text-white p-2 bg-success">
       <p>{{ label }}<br> <small style="color: #e0e0e0">Total: {{ total }}</small></p>
-       <div>
         <button class="btn btn-success" @click="addTransaction(1)">
           +
         </button>
-      </div>
     </h6>
     <h6 v-if="error" class="d-flex justify-content-between align-items-center text-white p-2 bg-danger">
       <p>{{ label }}<br> <small style="color: #e0e0e0">Total: {{ total }}</small></p>
-      <div>
         <button class="btn btn-danger" @click="addTransaction(2)">
           +
         </button>
-      </div>
     </h6>
-    <div class="p-2">
-        <slot></slot>
-    </div>
+    <slot></slot>
   </div>
 </template>
 
