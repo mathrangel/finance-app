6b6b4fe0122c210d72c2f9fc6a0cd72cca6a9538
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-04-30 19:54:43 -0300

    [client] add vue DatePicker

--- client/src/main.js
+++ client/src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
 
+import DatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 createApp(App)
 .use(router)
 .use(store)
+.use(DatePicker)
 .mount('#app')
