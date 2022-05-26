7f2b30294c51ecd4d003b205058f9afc452cd1b7
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2022-05-25 21:44:00 -0300

    [client] add new item on view initial

--- client/src/pages/Views/view_initial.vue
+++ client/src/pages/Views/view_initial.vue
@@ -2,7 +2,7 @@
   <div>
     <AppHeaderInitial></AppHeaderInitial>
     <div class="bg-success w-100">
-      <div style="max-width: 1080px" class="d-flex align-items-center justify-content-between text-white p-4 m-auto">
+      <div style="max-width: 1080px" class="d-flex align-items-center justify-content-between text-white p-5 m-auto mb-5">
         <div>
           <h1>Organize suas finanças<br>de forma mais rápida</h1>
           <p class="flex-wrap" style="max-width: 500px">
@@ -17,26 +17,40 @@
           </div>
         </div>
         <div>
-          <img width="300" src="/images/background.svg" alt="">
+          <img width="300" src="/images/background.svg">
         </div>
       </div>
     </div>
     <section style="max-width: 1080px" class="m-auto d-flex flex-column align-items-center py-3">
-      <!-- <div class="w-100 my-4">
+      <div class="d-flex justify-content-between align-items-center w-100 mb-5">
           <div>
-            TESTE
+            <img width="500" src="/images/background-1.svg">
           </div>
-          <div>
-            TESTE1
-          </div>
-      </div> -->
-      <div class="d-flex justify-content-around w-100 my-4">
+          <div class="d-flex flex-column justify-content-between" style="max-width: 500px">
+              <div class="mb-3">
+                <h5><strong>Saiba o destino de cada centavo</strong></h5>
+                <p class="flex-wrap" style="max-width: 500px"><small>Mantenha tudo sob controle informando sua renda e ganhos extras para ter um ponto de partida.</small></p>
+              </div>
+              <div class="w-100 border-bottom"></div>
+              <div class="my-3">
+                <h5><strong>Cadastre seus gastos de onde você quiser</strong></h5>
+              <p class="flex-wrap" style="max-width: 500px"><small>Garanta uma previsibilidade financeira poderosa cadastrando suas despesas em tempo real, de onde você estiver.</small></p>
+              </div>
+              <div class="w-100 border-bottom"></div>
+              <div class="my-3">
+                <h5><strong>Transforme em hábito</strong></h5>
+              <p class="flex-wrap" style="max-width: 500px"><small>Lance os gastos do dia a dia, acompanhe os relatórios sempre que possível e assuma o controle do seu dinheiro.</small></p>
+              </div>
+            </div>
+      </div>
+      <div class="w-100 border-bottom"></div>
+      <div class="d-flex justify-content-between w-100 mt-5">
           <AppCardInfo title="Controle" icon="/images/clipboard.svg" color="#157347" content="Com a Finance você controla suas finanças e organiza-as da melhor forma possível."/>
           <AppCardInfo title="Praticidade" icon="/images/calculator.svg" color="#157347" content="Você tem mais praticidade deixando de lado as planilhas e a calculadora, podendo usar qualquer dispositivo com acesso à internet."/>
           <AppCardInfo title="Planejamento" icon="/images/calendar.svg" color="#157347" content="A Finance oferece as melhores ferramentas para você se planejar e ter liberdade de poder desenvolver seu dinheiro."/>
       </div>
     </section>  
-    <AppFooterInitial v-if="false"/>
+    <AppFooterInitial/>
   </div>
 </template>
 
